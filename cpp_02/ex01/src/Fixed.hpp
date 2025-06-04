1c13db9ce70173cd8b82b25c84fa723d67d92bd7
nicolasque <quequecedo@gmail.com>
2025-06-04 12:49:05 +0200

    Add Fixed class implementation with constructors and methods; create Makefile and main.cpp

---
+++ cpp_02/ex01/src/Fixed.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nquecedo <nquecedo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/03 21:01:23 by nquecedo          #+#    #+#             */
+/*   Updated: 2025/06/04 12:48:58 by nquecedo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <cctype>
+#include <sstream>
+#include <cstdio>
+#include <cstdlib>
+
+class Fixed
+{
+private:
+	int _number;
+	static const int _fraction = 8;
+
+public:
+	Fixed();
+	Fixed(const int number);
+	Fixed(const Fixed &fixed);
+	Fixed &operator=(const Fixed &fixed);
+	~Fixed();
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+};
+
+#endif // FIXED_HPP
---
+++ cpp_02/ex01/src/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nquecedo <nquecedo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/03 13:25:03 by nquecedo          #+#    #+#             */
+/*   Updated: 2025/06/04 12:23:58 by nquecedo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main(void)
+{
+	Fixed a;
+	Fixed b(a);
+	Fixed c;
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return (0);
+}
