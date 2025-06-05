4704e14582305b062eee66bcd29ad7db4a7df11e
nicolasque <quequecedo@gmail.com>
2025-06-05 14:52:26 +0200

    Add Fixed class implementation with constructors, methods, and main function; create Makefile for project

---
+++ cpp_02/ex02/src/Fixed.cpp
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nquecedo <nquecedo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/03 21:01:33 by nquecedo          #+#    #+#             */
+/*   Updated: 2025/06/05 14:52:15 by nquecedo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor has been called" << std::endl;
+	this->_number = 0;
+}
+
+Fixed::Fixed(const int input)
+{
+	std::cout << "Int constructor called" << std::endl;
+	if (input >= MAX_FLO)
+	{
+		std::cout << "Input is out of range, fixedPointValue set to 0" << std::endl;
+		this->_number = 0;
+		return;
+	}
+	this->_number = round(input * pow(2, this->_fraction));
+}
+
+Fixed::Fixed(const float input)
+{
+	std::cout << "Float constructor called" << std::endl;
+	if (input >= MAX_FLO)
+	{
+		std::cout << "Input is out of range, fixedPointValue set to 0" << std::endl;
+		this->_number = 0;
+		return;
+	}
+	this->_number = round(input * (1 << this->_fraction));
+}
+
+Fixed::Fixed(const Fixed &fixed)
+{
+	std::cout << "Copy constructor has been called" << std::endl;
+	this->_number = fixed.getRawBits();
+}
+
+Fixed &Fixed::operator=(const Fixed &fixed)
+{
+	std::cout << "Copy asign operation has been called" << std::endl;
+	if (this != &fixed)
+		this->_number = fixed.getRawBits();
+	return (*this);
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Fixed destructor has been called" << std::endl;
+}
+
+int Fixed::getRawBits() const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_number);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	this->_number = raw;
+}
+
+float Fixed::toFloat() const
+{
+	return ((float)this->_number * (pow(2, -this->_fraction)));
+}
+
+int Fixed::toInt() const
+{
+	return (round(this->_number * (pow(2, -this->_fraction))));
+}
+
+std::ostream &operator<<(std::ostream& os, const Fixed &fixed)
+{
+	os << fixed.toFloat();
+	return (os);
+}
+
+
---
+++ cpp_02/ex02/src/Fixed.hpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nquecedo <nquecedo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/03 21:01:23 by nquecedo          #+#    #+#             */
+/*   Updated: 2025/06/04 17:56:41 by nquecedo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <cctype>
+#include <sstream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+
+#define FRACTIONAL_BITS 8
+#define MAX_FLO std::pow(2, (32 - FRACTIONAL_BITS)) - 1
+
+class Fixed
+{
+private:
+	int _number;
+	static const int _fraction = FRACTIONAL_BITS;
+
+public:
+	Fixed();
+	Fixed(const int number);
+	Fixed(const float number);
+	Fixed(const Fixed &fixed);
+	Fixed &operator=(const Fixed &fixed);
+	~Fixed();
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+	float toFloat(void) const;
+	int toInt(void) const;
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed& t_Fixed);
+
+#endif // FIXED_HPP
