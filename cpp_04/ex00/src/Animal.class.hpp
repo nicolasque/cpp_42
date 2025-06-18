8241c9baa1089adafa7773a97b3342b6fc9634ac
nicolasque <quequecedo@gmail.com>
2025-06-18 15:16:00 +0200

    Add initial implementation of Animal class and related files; create Cat and Dog class headers and source files; add Makefile for project structure

---
+++ cpp_04/ex00/src/Animal.class.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.class.hpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nquecedo <nquecedo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/17 22:08:01 by nquecedo          #+#    #+#             */
+/*   Updated: 2025/06/18 15:15:48 by nquecedo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cctype>
+#include <sstream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+
+class Animal
+{
+	protected:
+		std::string _type;
+
+	public:
+		
+
+};
