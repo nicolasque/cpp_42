1fb20f4eeaa3dd8888737c98b62b65c3f1636395
nicolasque <quequecedo@gmail.com>
2025-07-22 16:22:00 +0200

    Update Bureaucrat class: reorganize includes, adjust header comments, and refine exception classes

--- cpp_05/ex00/Bureaucrat.hpp
+++ cpp_05/ex00/Bureaucrat.hpp
@@ -6,44 +6,58 @@
 /*   By: nquecedo <nquecedo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 00:57:12 by nquecedo          #+#    #+#             */
-/*   Updated: 2025/07/22 12:19:37 by nquecedo         ###   ########.fr       */
+/*   Updated: 2025/07/22 15:53:41 by nquecedo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string>
-#include <iostream>
+#include <cctype>
 #include <fstream>
 #include <iomanip>
-#include <cctype>
+#include <iostream>
 #include <sstream>
+#include <string>
 
 #pragma once
 
+#define HIGHEST_GRADE 1
+#define LOWEST_GRADE 150
 
-class Bureaucrat {
-    private:
-        std::string _name;
-        int _range;
-		class GradeTooHighException : public std::exception {};
+class Bureaucrat
+{
+private:
+    std::string _name;
+    int _range;
+    class GradeTooHighException;
+    class GradeTooLowException;
 
-    public:
-        
-        Bureaucrat();
-        Bureaucrat(std::string name, int range);
-        Bureaucrat(const Bureaucrat& bureaucrat);
-        Bureaucrat &operator=(const Bureaucrat& bureaucrat);
+public:
+    Bureaucrat();
+    Bureaucrat(std::string name, int range);
+    Bureaucrat(const Bureaucrat &bureaucrat);
+    Bureaucrat &operator=(const Bureaucrat &bureaucrat);
 
-        std::string& operator<<(std::ostream& os);
-        std::string getName();
-        int getRange();
-        void incrementGrade();
-        void decreaseRange();
-    };
+    std::string getName();
+    int getRange();
+    void incrementGrade();
+    void decreaseRange();
+};
 
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &bureaucrat);
 
-class GradeTooHighException : public std::exception {
-	public:
-			const char* what() const noexcept override {
-				return ""
-			}
+class GradeTooHighException : public std::exception
+{
+public:
+    const char *what() const noexcept override
+    {
+        return ("Grade is too high! (maximun is 1)");
+    }
+};
+
+class GradeTooLowException : public std::exception
+{
+public:
+    const char *what() const noexcept override
+    {
+        return ("Grade is too low! (minimum is 150)");
+    }
 };
