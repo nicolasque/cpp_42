54899c73bac14f2a8f399ede5262c2e730e22fa2
nicolasque <quequecedo@gmail.com>
2025-07-22 12:18:22 +0200

    Update Bureaucrat.hpp header comments and reformat class member declarations

--- cpp_05/ex00/Bureaucrat.hpp
+++ cpp_05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: nquecedo <nquecedo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 00:57:12 by nquecedo          #+#    #+#             */
-/*   Updated: 2025/07/21 19:39:47 by nquecedo         ###   ########.fr       */
+/*   Updated: 2025/07/22 12:19:37 by nquecedo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 
 #pragma once
 
+
 class Bureaucrat {
     private:
         std::string _name;
@@ -26,13 +27,18 @@ class Bureaucrat {
 		class GradeTooHighException : public std::exception {};
 
     public:
+        
         Bureaucrat();
         Bureaucrat(std::string name, int range);
         Bureaucrat(const Bureaucrat& bureaucrat);
         Bureaucrat &operator=(const Bureaucrat& bureaucrat);
 
         std::string& operator<<(std::ostream& os);
-};
+        std::string getName();
+        int getRange();
+        void incrementGrade();
+        void decreaseRange();
+    };
 
 
 class GradeTooHighException : public std::exception {
