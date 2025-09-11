809ab8673028df7185b14c0dcaef5eae5fad0986
nicolasque <quequecedo@gmail.com>
2025-09-11 19:15:19 +0200

    Add whatever.hpp with template functions for swap, min, and max
    - Implemented a template function `swap` to exchange values of two variables.
    - Added template functions `min` and `max` to return the minimum and maximum of two values respectively.
    - Included necessary headers for functionality and type support.

--- cpp_07/ex00/src/main.cpp
+++ cpp_07/ex00/src/main.cpp
@@ -6,7 +6,21 @@
 /*   By: nquecedo <nquecedo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 21:48:17 by nquecedo          #+#    #+#             */
-/*   Updated: 2025/09/10 21:48:18 by nquecedo         ###   ########.fr       */
+/*   Updated: 2025/09/11 17:45:31 by nquecedo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "whatever.hpp"
+
+int main()
+{
+    int a = 5;
+    int b = 2;
+
+    std::cout << "Min: " << min(a,b) << std::endl;
+    std::cout << "Max: " << max(a,b) << std::endl;
+
+    std::cout << "A: " << a << " B: " << b << std::endl;
+    std::cout << "Swap" << std::endl;
+    std::cout << "A: " << a << " B: " << b << std::endl;
+}
---
+++ cpp_07/ex00/src/whatever.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nquecedo <nquecedo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/11 17:37:11 by nquecedo          #+#    #+#             */
+/*   Updated: 2025/09/11 18:29:14 by nquecedo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <cctype>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <unistd.h>
+#include <cmath>
+#include <limits>
+#include <algorithm>
+#include <cstring>
+#include <algorithm>
+#include <typeinfo>
+
+template <class T>
+void swap(T &a, T &b)
+{
+    T *temp = &a;
+    &a = &b;
+    &b = &temp;
+}
+
+template <class T>
+T min(T a, T b)
+{
+    if (a >= b)
+        return (b);
+    return (a);
+}
+
+template <class T>
+T max(T a, T b)
+{
+    if (b >= a)
+        return (b);
+    return (a);
+}
