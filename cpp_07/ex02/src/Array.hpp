d46964ba9ddd4fdfc23d0e5ce6dc0110a2874f17
nicolasque <quequecedo@gmail.com>
2025-09-11 22:33:17 +0200

    dfdsfdsfsdfdsf

--- cpp_07/ex02/src/Array.hpp
+++ cpp_07/ex02/src/Array.hpp
@@ -1,19 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nquecedo <nquecedo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/10 21:48:17 by nquecedo          #+#    #+#             */
-/*   Updated: 2025/09/11 22:28:12 by nquecedo         ###   ########.fr       */
+/*   Created: 2025/09/11 20:18:31 by nquecedo          #+#    #+#             */
+/*   Updated: 2025/09/11 20:21:08 by nquecedo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Array.hpp"
-
-int main()
-{
-    int *a = new int();
-    std::cout << *a << std::endl;
-}
+#pragma once
+#include <cctype>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <unistd.h>
+#include <cmath>
+#include <limits>
+#include <algorithm>
+#include <cstring>
+#include <algorithm>
+#include <typeinfo>
