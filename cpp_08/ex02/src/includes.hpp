d5f56365d8b192782036135d71f7d64a61e8cb7d
nicolasque <quequecedo@gmail.com>
2025-12-01 15:30:36 +0100

    Add Makefile and initial source files for project setup

---
+++ cpp_08/ex02/src/includes.hpp
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   includes.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nquecedo <nquecedo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/11 17:37:11 by nquecedo          #+#    #+#             */
+/*   Updated: 2025/12/01 14:20:47 by nquecedo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#pragma once
+#include <cctype>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <unistd.h>
+#include <cmath>
+#include <limits>
+#include <algorithm>
+#include <cstring>
+#include <algorithm>
+#include <typeinfo>
+#include <vector>
+#include <deque>
+#include <list>
+#include <stdexcept>
+#include <limits>
+
+
+// template <class T>
+// void swap(T &a, int nbr)
+// {
+//     T temp = a;
+//     a = b;
+//     b = temp;
+// }
+
+template <class T>
+T fill_container(int *arr, int size)
+{
+	T v;
+	for (int i = 0; i < size ; i++)
+	{
+		v.push_back(arr[i]);
+	}
+	return v;
+};
+
+
+template <typename T>
+typename T::iterator easyfind(T &cont, int nbr)
+{
+	(void) nbr;
+    typename T::iterator it = cont.begin();
+
+	for (; it != cont.end(); it++)
+	{
+		// std::cout <<  *it << " ";
+		if (*it == nbr)
+			return it;
+	}
+
+	throw std::invalid_argument("nbr not in the conntainer");
+};
+
+// template <typename T>
+// void printList(T &cont, unsigned int size)
+// {
+	
+// }
